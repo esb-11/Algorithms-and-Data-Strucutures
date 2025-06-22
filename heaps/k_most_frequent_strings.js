b9da6330d8577ddbefbd419ddd0c9dd1066bfc06
Eduardo <198397564+esb-11@users.noreply.github.com>
2025-06-22 17:01:39 -0300

    solve k most frequent string using my heap implementation

--- heaps/k_most_frequent_strings.js
+++ heaps/k_most_frequent_strings.js
@@ -1,44 +1,95 @@
 const input = ["go", "coding", "byte", "byte", "go", "interview", "go"];
 
-console.log(mostFrequent(2, input));
+class Pair {
+  constructor(str, freq) {
+    this.str = str;
+    this.freq = freq;
+  }
+}
 
-function mostFrequent(k, array) {
+console.log(mostFrequentString(input, 2));
+
+function mostFrequentString(array, k) {
   const hashmap = {};
-  const frequencies = [];
 
-  for (const string of array) {    
-    if (typeof hashmap[string] == "undefined") {      
-      hashmap[string] = frequencies.length;
-      
-      frequencies.push({str: string, freq: 0});
-    }
-
-    frequencies[hashmap[string]].freq += 1;
+  for (const string of array) {
+    hashmap[string] = hashmap[string] ? hashmap[string] + 1 : 1;
   }
 
-  const heap = heapify(frequencies);
+  const max_heap = [];
+  for (const str in hashmap) {
+    const pair = new Pair(str, hashmap[str]);
+    max_heap.push(pair);
+  }
 
-  return heap;
+  heapify(max_heap);
+
+  let result;
+  for (let i = 0; i < k; i++) {
+    result = pop(max_heap);
+  }
+
+  return result;
 }
 
 function heapify(array) {
-  let head = makeNode(array[0]);
+  console.log(array);
   
-  for (let i = 1; i < array.length; i++) {
-    const node = makeNode(array[i]);
-    if (node.freq > head.freq) {
-      node.left = head;
-      head = node;
-    } else if (node.freq === head.freq) {
-      
+  const stack = [Math.floor(array.length) - 1];
+
+  while (stack.length !== 0) {
+    const parent = stack.pop();
+
+    const left_child = 2 * parent + 1;
+    const right_child = 2 * parent + 2;
+
+    let largest = parent;
+    if (
+      left_child < array.length &&
+      array[left_child].freq > array[largest].freq
+    ) {
+      largest = left_child;
+    }
+    if (
+      right_child < array.length &&
+      array[right_child].freq > array[largest].freq
+    ) {
+      largest = right_child;
+    }
+
+    [array[parent], array[largest]] = [array[largest], array[parent]];
+
+    if (parent > 0) stack.push(parent - 1);
+    if (parent !== largest) {
+      stack.push(largest);
     }
   }
 
-  return head
+  return array;
 }
 
-function makeNode(obj) {
-  obj.left = null;
-  obj.right = null;
-  return obj;
+function pop(heap) {
+  const oldRoot = heap[0];
+  heap[0] = heap.pop();
+
+  const stack = [0];
+  while (stack.length > 0) {
+    const parent = stack.pop();
+    
+    const left_child = 2 * parent + 1;
+    const right_child = 2 * parent + 2;
+
+    let largest = parent;
+    if (left_child < heap.length && heap[left_child].freq > heap[largest].freq) {
+      largest = left_child;
+    }
+    if (right_child < heap.length && heap[right_child].freq > heap[largest].freq) {
+      largest = right_child;
+    }
+    
+    [heap[parent], heap[largest]] = [heap[largest], heap[parent]];
+    if (parent !== largest && largest < heap.length) stack.push(largest);
+  }
+
+  return oldRoot;
 }
